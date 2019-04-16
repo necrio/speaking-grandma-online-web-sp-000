eae137fec25235384c4cf3b4dbc974da2015464c
Keith Isham <kisham8899@gmail.com>
2019-04-16 20:19:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == "#{phrase}"
     return puts "HUH?! SPEAK UP, SONNY!"
   elsif
-
+    "#{phrase}" == "#{phrase}.upcase"
+    return puts "NO, NOT SINCE 1938!"
 
 
 
